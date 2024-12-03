7c57805fa7efe1c95456df4327efda4a985b1025
FarhanK340 <farhankashif6@gmail.com>
2024-12-03 12:10:18 +0500

    Lab 11 Implementation

--- src/expressivo/Expression.java
+++ src/expressivo/Expression.java
@@ -1,12 +1,32 @@
 package expressivo;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import expressivo.parser.ExpressionLexer;
+import expressivo.parser.ExpressionParser;
+
 /**
  * An immutable data type representing a polynomial expression of:
  *   + and *
  *   nonnegative integers and floating-point numbers
  *   variables (case-sensitive nonempty strings of letters)
+ * 
+ * <p>PS3 instructions: this is a required ADT interface.
+ * You MUST NOT change its name or package or the names or type signatures of existing methods.
+ * You may, however, add additional methods, or strengthen the specs of existing methods.
+ * Declare concrete variants of Expression in their own Java source files.
  */
 public interface Expression {
+    
+    // Datatype definition
+    //   Expression = Number(n: int) + Variable(var: String) + 
+    //                Operation(op: String, left: Expression, right: Expression)
+    
     /**
      * Parse an expression.
      * @param input expression to parse, as defined in the PS3 handout.
@@ -14,9 +34,19 @@ public interface Expression {
      * @throws IllegalArgumentException if the expression is invalid
      */
     public static Expression parse(String input) {
-        return ExpressionParser.parse(input); // Delegates to the ExpressionParser.
+        CharStream stream = new ANTLRInputStream(input);
+        ExpressionLexer lexer = new ExpressionLexer(stream);
+        lexer.reportErrorsAsExceptions();
+        TokenStream tokens = new CommonTokenStream(lexer);
+        ExpressionParser parser = new ExpressionParser(tokens);
+        parser.reportErrorsAsExceptions();
+        
+        ParseTree tree = parser.root();
+        ExpressionMaker maker = new ExpressionMaker();
+        new ParseTreeWalker().walk(maker, tree);
+        return maker.getExpression();
     }
-
+    
     /**
      * @return a parsable representation of this expression, such that
      * for all e:Expression, e.equals(Expression.parse(e.toString())).
@@ -31,7 +61,7 @@ public interface Expression {
      */
     @Override
     public boolean equals(Object thatObject);
-
+    
     /**
      * @return hash code value consistent with the equals() definition of structural
      * equality, such that for all e1,e2:Expression,
@@ -39,4 +69,13 @@ public interface Expression {
      */
     @Override
     public int hashCode();
+    
+    /**
+     * Differentiate an expression with respect to a variable.
+     * @param variable the variable to differentiate by, a case-sensitive nonempty string of letters.
+     * @return expression's derivative with respect to variable.
+     */
+    public Expression differentiate(String variable);
+
+    
 }
